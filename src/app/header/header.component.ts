e605ce7169d81a9749ebf30756ccd0dae1de5700
Jinal Shah <jinal28m.shah@gmail.com>
2023-08-30 21:09:06 +0530

    comment

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-slogan:any="Your one stop shop for everything";
+slogan:string="Your one stop shop for everything";
+source:string="./assets/1.jfif";
 }
--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.css']
 })
-export class NotificationComponent {
+export class NotificationComponent  implements OnInit{
+  constructor(){}
+  ngOnInit(): void {
+    
+  }
+  displayNotification: boolean=false;
 
 }
